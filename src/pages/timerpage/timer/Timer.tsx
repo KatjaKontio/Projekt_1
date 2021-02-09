f66fc7f2a40302b6c1c7fe397fd28a6ea9937a95
Katja Kontio <kontiokatja@gmail.com>
2021-02-09 11:42:43 +0100

    m

--- src/pages/timerpage/timer/Timer.tsx
+++ src/pages/timerpage/timer/Timer.tsx
@@ -1,28 +1,5 @@
+
 export const Timer = () => {
-
-/*document.addEventListener('DOMContentLoaded', () => {
-        const timeLeftDisplay = document.querySelector('#time-left')
-        const startBtn = document.querySelector('#start-button')
-        
-        let timeLeft  = 10
-
-        function countDown() {
-            setInterval(function (timeLeftDisplay) {
-                if (timeLeft <= 0) {
-                    clearInterval(timeLeft = 0)
-                }
-                timeLeftDisplay.innerHTML = timeLeftDisplay
-                timeLeft -= 1
-            }, 1000)
-        }
-    
-
-
-    startBtn.addEventListener('click', countDown)
-
-    })
-*/
-
 
 
     return (
