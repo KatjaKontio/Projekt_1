7c73d36efeeee34a6136afd1699c91afabda3aba
Katja Kontio <kontiokatja@gmail.com>
2021-02-09 16:38:39 +0100

    api

--- src/pages/exercisespage/exercisescomponent/ExercisesComponent.tsx
+++ src/pages/exercisespage/exercisescomponent/ExercisesComponent.tsx
@@ -1,26 +1,41 @@
 import './ExercisesComponent.css'
-import Axios, { AxiosResponse } from 'axios'
 import { useState, useEffect } from 'react'
+import StarWarsAPIService from '../../../shared/api/service/StarWarsAPIService'
 
 
 export const ExercisesComponent = () => {
-    const [starwarsData, setStarwarsData] = useState<AxiosResponse>()
+    const [starwarsData, setStarwarsData] = useState<any>()
+    const [count, setCount] = useState(1)
 
-    const getDataFromStarWarsAPI = () => {
-        Axios.get('https://swapi.dev/api/people/1/')
-            .then(response => setStarwarsData(response))
-            .catch(error => console.log(error))
+    const makeSureCountWillNeverGoBelowValue1 = () => {
+        (count <= 1) ? setCount(1) : setCount(count - 1)
     }
+
+    const getDataFromStarWarsAPI = async () => {
+        try {
+            const response = await StarWarsAPIService.getStarWarsCharacter(count)
+        } catch (error) {
+            console.log(error)
+
+        }
+    }
+
 
     useEffect(() => {
         getDataFromStarWarsAPI()
-    }, [])
+    }, [count])
 
     return (
         <div className='exercisesWrapper'>
 
-            <h1>Exercises</h1>
-            <button onClick={() => console.log(starwarsData)}>Test Api Call</button>
+            <h1>Name: {starwarsData?.data?.name}</h1>
+            <h1>Hair Color: {starwarsData?.data?.hair_color}</h1>
+            <h1>Gender: {starwarsData?.data?.gender}</h1>
+            <h1>Birth year: {starwarsData?.data?.birth_year}</h1>
+            <h1>Height: {starwarsData?.data?.height}</h1>
+            <button onClick={() => makeSureCountWillNeverGoBelowValue1()}>Get previous character</button>
+            <button onClick={() => setCount(count + 1)}>Get next character</button>
         </div>
     )
 }
+
